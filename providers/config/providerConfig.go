c14dc9dfd6e490040c94b7a68a36ed7edfe63881
max19931 <53226461+max19931@users.noreply.github.com>
2020-02-13 08:42:35 +0100

    strip comments

--- providers/config/providerConfig.go
+++ providers/config/providerConfig.go
@@ -1,7 +1,3 @@
-// Package config provides functions for reading and parsing the provider credentials json file.
-// It cleans nonstandard json features (comments and trailing commas), as well as replaces environment variable placeholders with
-// their environment variable equivalents. To reference an environment variable in your json file, simply use values in this format:
-//    "key"="$ENV_VAR_NAME"
 package config
 
 import (
@@ -13,13 +9,10 @@ import (
 	"github.com/DisposaBoy/JsonConfigReader"
 	"github.com/TomOnTime/utfutil"
 )
-
-// LoadProviderConfigs will open the specified file name, and parse its contents. It will replace environment variables it finds if any value matches $[A-Za-z_-0-9]+
 func LoadProviderConfigs(fname string) (map[string]map[string]string, error) {
 	var results = map[string]map[string]string{}
 	dat, err := utfutil.ReadFile(fname, utfutil.POSIX)
 	if err != nil {
-		// no creds file is ok. Bind requires nothing for example. Individual providers will error if things not found.
 		if os.IsNotExist(err) {
 			fmt.Printf("INFO: Config file %q does not exist. Skipping.\n", fname)
 			return results, nil
