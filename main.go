2105c3983b570af8f6c91723f781b1fb1f97befd
max19931 <53226461+max19931@users.noreply.github.com>
2020-02-13 08:40:10 +0100

    comment strip

--- main.go
+++ main.go
@@ -10,31 +10,21 @@ import (
 	"github.com/StackExchange/dnscontrol/v2/commands"
 	_ "github.com/StackExchange/dnscontrol/v2/providers/_all"
 )
-
-//go:generate go run build/generate/generate.go build/generate/featureMatrix.go
-
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	os.Exit(commands.Run(versionString()))
 }
-
-// Version management. 2 Goals:
-// 1. Someone who just does "go get" has at least some information.
-// 2. If built with build/build.go, more specific build information gets put in.
-// Update the number here manually each release, so at least we have a range for go-get people.
 var (
 	SHA       = ""
 	Version   = "2.10.0"
 	BuildTime = ""
 )
-
-// printVersion prints the version banner.
 func versionString() string {
 	var version string
 	if SHA != "" {
 		version = fmt.Sprintf("%s (%s)", Version, SHA)
 	} else {
-		version = fmt.Sprintf("%s-dev", Version) // no SHA. '0.x.y-dev' indicates it is run from source without build script.
+		version = fmt.Sprintf("%s-dev", Version)
 	}
 	if BuildTime != "" {
 		i, err := strconv.ParseInt(BuildTime, 10, 64)
